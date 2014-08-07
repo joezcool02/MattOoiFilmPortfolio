f355155e23a811a7331b3f7992a58c86405bee76
Duckzbug <domduxbury@hotmail.com>
2014-08-07 18:42:57 +0100

    Java script arrays started but not complete

--- script/mediaPlayer.js
+++ script/mediaPlayer.js
@@ -15,8 +15,31 @@ function loadVideo() {
    }
 }
 
+// checks if the video type is supported
 function canPlayVideo(ext) {
    var ableToPlay = mediaPlayer.canPlayType('video/' + ext);
    if (ableToPlay == '') return false;
    else return true;
+}
+
+// Gets the info for a video
+function getInfo(fileName) {
+    var fileReader = FileReader();
+    var file = document.getElementsByName('../video/eiffelTowerInfo')
+}
+
+// Create an array to hold imageData
+
+var imageData = new Array(10)
+
+createTwoDimensionalArray(3);
+
+// This function creates our two dimensional array
+
+function createTwoDimensionalArray(arraySize) {
+
+for (i = 0; i < imageData.length; ++ i)
+
+imageData[i] = new Array(arraySize);
+
 }
